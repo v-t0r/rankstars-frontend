e6ff64cbaa08251f0a4cc35727687814596a0458
Vitor <vitorlemes.com@hotmail.com>
2025-05-11 20:34:05 -0300

    Improved the image carousel's behavior

--- src/components/imageCarousel/ImageCarousel.jsx
+++ src/components/imageCarousel/ImageCarousel.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { imageBackendUrl } from "../../utils/constants"
 
 import classes from "./ImageCarousel.module.css"
@@ -8,17 +8,27 @@ import { motion } from "framer-motion"
 export default function ImageCarousel({onClose, images = []}) {
     
     const [selectedImage, setSelectedImage] = useState(0)
+    const imageRefs = useRef([])
 
 
-    const arrowButtonVariants = {
-        hover: {scale: 1.3}
+    const arrowRightButtonVariants = {
+        hover: {scale: 1.3, x: 5}
+    }
+
+    const arrowLeftButtonVariants = {
+        hover: {scale: 1.3, x: -5}
+    }
+
+    function handleChangeSelectedImage(newIndex){
+        setSelectedImage(newIndex)
+        imageRefs.current[newIndex].scrollIntoView({behavior: "smooth", inline: "center"})
     }
 
     return <div className={classes["main-container"]}>
         <button 
             type="button"
             className="negative-button" 
-            style={{alignSelf: "end", width: "fit-content"}}
+            style={{alignSelf: "end", width: "fit-content", fontSize: "20px"}}
             onClick={onClose}
         >
             X
@@ -28,31 +38,30 @@ export default function ImageCarousel({onClose, images = []}) {
             <motion.div 
                 whileHover="hover" 
                 className={`${classes["arrow-button"]} ${classes["left-button"]}`}
-                onClick={() => setSelectedImage(prev => prev === 0 ? (images.length-1) : prev-1)}
+                onClick={() => handleChangeSelectedImage(selectedImage === 0 ? (images.length-1) : selectedImage-1)}
             >
-                <motion.span variants={arrowButtonVariants} className="material-symbols-outlined">
-                    arrow_back_ios
-                </motion.span>
+                <motion.span variants={arrowLeftButtonVariants} className="material-symbols-outlined">arrow_back_ios</motion.span>
             </motion.div>
             <img src={`${imageBackendUrl}/${images[selectedImage]}`} alt="selected image"/>
             <motion.div 
                 whileHover="hover" 
                 className={`${classes["arrow-button"]} ${classes["right-button"]}`}
-                onClick={() => setSelectedImage(prev => (prev+1) % images.length) }
+                onClick={() => handleChangeSelectedImage( (selectedImage+1) % images.length) }
             >
-                <motion.span variants={arrowButtonVariants} className="material-symbols-outlined">
-                    arrow_forward_ios
-                </motion.span>
+                <motion.span variants={arrowRightButtonVariants} className="material-symbols-outlined">arrow_forward_ios</motion.span>
             </motion.div>
         </div>
         
         <div className={classes["preview-images-section"]}>
             {images.map((image, index) => {
-                return <div key={index} className={`${classes["image-container"]} ${selectedImage === index ? classes["selected-image"] : undefined}`}>
+                return <div key={index} className={classes["image-container"]}>
                     <img 
+                        ref={(e) => imageRefs.current[index] = e}
                         src={`${imageBackendUrl}/${image}`} 
                         alt={`image ${index}`} 
-                        onClick={() => setSelectedImage(index)}
+                        onClick={() => handleChangeSelectedImage(index)}
+                        className={`${selectedImage === index ? classes["selected-image"] : undefined}`}
+                        
                     />
                 </div>
             })}
